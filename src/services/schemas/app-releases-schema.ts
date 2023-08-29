af6f35ea7d0459269bf279ec0f4d74fb665e5624
Your Full Name <your-user@monday.com>
2023-08-29 15:41:57 +0300

    Feat: support builds

---
+++ src/services/schemas/app-releases-schema.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+import { baseResponseHttpMetaDataSchema } from 'services/schemas/api-service-schemas';
+import { appReleaseIdSchema, appVersionIdSchema } from 'services/schemas/general-schemas';
+
+export const appReleaseSchema = z.object({
+  id: appReleaseIdSchema,
+  // eslint-disable-next-line camelcase
+  app_version_id: appVersionIdSchema,
+  kind: z.string(),
+  category: z.string(),
+  state: z.string(),
+  data: z
+    .object({
+      url: z.string().optional(),
+      latestUrl: z.string().optional(),
+      liveUrl: z.string().optional(),
+      deploymentState: z.string().optional(),
+      sourceUrl: z.string().optional(),
+      microFrontendName: z.string().optional(),
+    })
+    .optional(),
+});
+
+export const appReleasesSchema = z
+  .object({
+    appReleases: z.array(appReleaseSchema),
+  })
+  .merge(baseResponseHttpMetaDataSchema);
+
+export type AppReleasesResponse = z.infer<typeof appReleasesSchema>;
+export type AppRelease = z.infer<typeof appReleaseSchema>;
