099b5ea1185d9dcf05f7dd38a2c386ff0e3c9cee
Maor Barazani <maorba@monday.com>
2023-11-15 09:49:27 +0200

    finalize create-tunnel service logic and logs

--- src/services/tunnel-service.ts
+++ src/services/tunnel-service.ts
@@ -11,10 +11,7 @@ import { TunnelAuthToken } from 'types/services/push-service';
 import logger from 'utils/logger';
 import { appsUrlBuilder } from 'utils/urls-builder';
 
-export const generateTunnelingAuthToken = async (
-  ctx: TunnelCommandTasksContext,
-  task: ListrTaskWrapper<TunnelCommandTasksContext, any>,
-) => {
+export const generateTunnelingAuthToken = async (ctx: TunnelCommandTasksContext) => {
   const DEBUG_TAG = 'generate_tunneling_auth_token';
   try {
     const baseUrl = generateTunnelingTokenUrl();
@@ -34,8 +31,6 @@ export const generateTunnelingAuthToken = async (
     ctx.authToken = response.token;
     ctx.tunnelDomain = response.domain;
 
-    task.output = `Creating a tunnel from "${ctx.tunnelDomain}" to "http://localhost:${ctx.tunnelPort}"`; // FIXME: not working
-
     return {
       token: response.token,
       domain: response.domain,
@@ -46,34 +41,31 @@ export const generateTunnelingAuthToken = async (
       throw error;
     }
 
-    throw new Error('Failed to build remote location for upload.');
+    throw new Error('Failed to generate tunneling auth token.');
   }
-
-  // return mockImpl(ctx, task); // FIXME: remove
 };
 
 export const createTunnelConnection = async (
   ctx: TunnelCommandTasksContext,
   task: ListrTaskWrapper<TunnelCommandTasksContext, any>,
 ) => {
-  // FIXME: maor: remove all logs and redundant code
-  // FIXME: maor: add logger
   const DEBUG_TAG = 'create_tunnel_connection';
 
   try {
     const forwardingAddress = `http://localhost:${ctx.tunnelPort}`;
+    logger.debug(`forwarding address: ${forwardingAddress}`, DEBUG_TAG);
     const session = await new ngrok.SessionBuilder().authtoken(String(ctx.authToken)).connect();
+    logger.debug(`session created`, DEBUG_TAG);
     const tunnel = await session.httpEndpoint().domain(String(ctx.tunnelDomain)).forwardsTo(forwardingAddress).listen();
-    const logs = [
-      `Ingress established at: ${String(tunnel.url())}`,
-      `forwarding traffic to: ${forwardingAddress}`,
-      `Terminate this process to close the tunnel connection`,
-    ];
-    task.output = logs.join('\n');
-    await tunnel.forward(forwardingAddress);
+    logger.debug(`tunnel created`, DEBUG_TAG);
+
+    task.output = `Ingress established at: ${String(tunnel.url())} --> forwarding traffic to: ${forwardingAddress}
+      \nTerminate this process to close the tunnel connection`;
+
+    await tunnel.forward(forwardingAddress); // actually opens the tunnel, and will "hang" open until terminated
   } catch (error) {
-    // TODO: what to expose to the user? the error comes from ngrok
-    console.log('Error setting up tunnel:', error);
-    // TODO: maor: handle error, proccess.exit?
+    logger.debug(error, DEBUG_TAG);
+    logger.error('Failed to establish tunnel connection', DEBUG_TAG);
+    process.exit(1);
   }
 };
