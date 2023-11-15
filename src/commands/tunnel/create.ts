77b4701a73acc72fdd0f9a571bc800ee06cacaf0
Maor Barazani <maorba@monday.com>
2023-11-15 09:27:20 +0200

    refactor command as "tunnel:create" set 8080 as default port

--- src/commands/tunnel/create.ts
+++ src/commands/tunnel/create.ts
@@ -2,27 +2,31 @@ import { Flags } from '@oclif/core';
 import { Listr } from 'listr2';
 
 import { AuthenticatedCommand } from 'commands-base/authenticated-command';
-import { APP_ID_TO_ENTER, PORT_TO_ENTER } from 'consts/messages';
+import { APP_ID_TO_ENTER } from 'consts/messages';
 import { DynamicChoicesService } from 'services/dynamic-choices-service';
-import { PromptService } from 'services/prompt-service';
 import { createTunnelConnection, generateTunnelingAuthToken } from 'services/tunnel-service';
 import { TunnelCommandTasksContext } from 'types/commands/tunnel';
 import { validateStringAsSafeInt } from 'types/utils/validation';
 import logger from 'utils/logger';
 
-export default class AppTunnel extends AuthenticatedCommand {
-  // TODO: Maor: check -help to see if it's working and looking properly
-  static description = 'Create an app tunnel to expose code running on the local machine.';
+export default class TunnelCreate extends AuthenticatedCommand {
+  static description = 'Create a networking tunnel to publicly expose code running on the local machine.';
 
   static withPrintCommand = false;
 
-  static examples = ['<%= config.bin %> <%= command.id %>', '<%= config.bin %> <%= command.id %> -a APP_ID_FOR_TUNNEL'];
+  static examples = [
+    '<%= config.bin %> <%= command.id %>',
+    '<%= config.bin %> <%= command.id %> -p PORT_FOR_TUNNEL',
+    '<%= config.bin %> <%= command.id %> -a APP_ID_FOR_TUNNEL',
+    '<%= config.bin %> <%= command.id %> -p PORT_FOR_TUNNEL -a APP_ID_FOR_TUNNEL',
+  ];
 
-  static flags = AppTunnel.serializeFlags({
+  static flags = TunnelCreate.serializeFlags({
     port: Flags.integer({
       char: 'i',
       aliases: ['p'],
-      description: PORT_TO_ENTER,
+      description: 'Port to forward tunnel traffic to.',
+      default: 8080,
     }),
     appId: Flags.integer({
       char: 'i',
@@ -31,16 +35,13 @@ export default class AppTunnel extends AuthenticatedCommand {
     }),
   });
 
-  DEBUG_TAG = 'app_tunnel';
+  DEBUG_TAG = 'tunnel_create';
 
   public async run(): Promise<void> {
     try {
-      const { flags } = await this.parse(AppTunnel);
-      let { port, appId } = flags;
-
-      if (!port) {
-        port = await PromptService.promptInputNumber(PORT_TO_ENTER, true);
-      }
+      const { flags } = await this.parse(TunnelCreate);
+      let { appId } = flags;
+      const { port } = flags;
 
       if (!appId) {
         appId = await DynamicChoicesService.chooseApp(true);
