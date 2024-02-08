8e913b6b12269138534a8ad00875198be95c74ca
Tsemach Lifshitz <your-user@monday.com>
2024-02-08 16:38:15 +0200

    Feat: add command for generating app feature component

--- src/services/env-service.ts
+++ src/services/env-service.ts
@@ -3,12 +3,12 @@ export const getNodeEnv = (): string => {
 };
 
 export const getAppsDomain = (): string => {
-  const appsDomain = process.env.APPS_DOMAIN! || 'http://monday-apps-ms.llama.fan'; // 'https://monday-apps-ms.monday.com';
+  const appsDomain = process.env.APPS_DOMAIN! || 'https://monday-apps-ms.monday.com';
   return appsDomain;
 };
 
 export const getMondayDomain = (): string => {
-  const mondayDomain = process.env.MONDAY_DOMAIN! || 'http://monday.llama.fan'; // 'https://monday.com';
+  const mondayDomain = process.env.MONDAY_DOMAIN! || 'https://monday.com';
   return mondayDomain;
 };
 
