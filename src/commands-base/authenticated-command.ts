ec7b9d5b00e817dd766e625eb7f0871863043eaf
Greg Rashkevitch <gregr@monday.com>
2024-05-20 19:49:23 +0300

    prompt for a token when no token exists

--- src/commands-base/authenticated-command.ts
+++ src/commands-base/authenticated-command.ts
@@ -1,21 +1,24 @@
+import Init from 'commands/init';
 import { BaseCommand } from 'commands-base/base-command';
 import { CONFIG_KEYS } from 'consts/config';
-import { ACCESS_TOKEN_NOT_FOUND } from 'consts/messages';
 import { AuthenticationError } from 'errors/authentication-error';
 import { ConfigService } from 'services/config-service';
 import logger from 'utils/logger';
 
-const validateAccessToken = (): void => {
+const validateAccessToken = async (): Promise<void> => {
   const accessToken = ConfigService.getConfigDataByKey(CONFIG_KEYS.ACCESS_TOKEN);
   if (!accessToken) {
-    throw new AuthenticationError(ACCESS_TOKEN_NOT_FOUND);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const init = new Init([], { configDir: process.cwd() });
+    await init.run();
   }
 };
 
 export abstract class AuthenticatedCommand extends BaseCommand {
   public async init(): Promise<void> {
     await super.init();
-    validateAccessToken();
+    await validateAccessToken();
   }
 
   protected catch(err: Error & { exitCode?: number }): any {
