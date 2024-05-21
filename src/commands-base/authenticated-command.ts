807ac4d05c513c656bfcccacfb84b40b68f64716
Greg Rashkevitch <gregr@monday.com>
2024-05-21 09:44:02 +0300

    use oclif to run commands properly

--- src/commands-base/authenticated-command.ts
+++ src/commands-base/authenticated-command.ts
@@ -1,4 +1,5 @@
-import Init from 'commands/init';
+import { Config } from '@oclif/core';
+
 import { BaseCommand } from 'commands-base/base-command';
 import { CONFIG_KEYS } from 'consts/config';
 import { ACCESS_TOKEN_NOT_FOUND_RUNNING_INIT } from 'consts/messages';
@@ -6,21 +7,18 @@ import { AuthenticationError } from 'errors/authentication-error';
 import { ConfigService } from 'services/config-service';
 import logger from 'utils/logger';
 
-const validateAccessToken = async (): Promise<void> => {
+const validateAccessToken = async (config: Config): Promise<void> => {
   const accessToken = ConfigService.getConfigDataByKey(CONFIG_KEYS.ACCESS_TOKEN);
   if (!accessToken) {
     logger.success(ACCESS_TOKEN_NOT_FOUND_RUNNING_INIT);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const init = new Init([], { configDir: process.cwd() });
-    await init.run();
+    await config.runCommand('init', ['--local']);
   }
 };
 
 export abstract class AuthenticatedCommand extends BaseCommand {
   public async init(): Promise<void> {
     await super.init();
-    await validateAccessToken();
+    await validateAccessToken(this.config);
   }
 
   protected catch(err: Error & { exitCode?: number }): any {
