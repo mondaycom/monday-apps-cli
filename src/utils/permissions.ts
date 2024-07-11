d17106fccdd48e7a06904948e0eef8706f7676ff
Maor Barazani <maorba@monday.com>
2024-07-11 15:18:51 +0300

    [beta] remove multi-region env-var protection create a new beta version

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -1,13 +1,8 @@
-import { getIsSupportMultiRegion } from 'services/env-service';
 import { Permissions } from 'types/utils/permissions';
 
 export const isPermitted = (permission: Permissions): boolean => {
   if (!permission) {
     return false;
-  }
-
-  if (permission === Permissions.MCODE_MULTI_REGION) {
-    return getIsSupportMultiRegion();
   }
 
   return false;
