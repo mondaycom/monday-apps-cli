8f6221c6b39349a9e1f8a9664ff8ab563bf421ec
Shaharshaki2 <shaharsh@monday.com>
2025-02-09 13:11:00 +0200

    support cdn uploading for client side (#115)
    * support cdn uploading for client side
    
    * fixing comments
    
    * fix
    
    * upgrade version
    
    * fix

--- src/commands/code/push.ts
+++ src/commands/code/push.ts
@@ -5,7 +5,7 @@ import { APP_ID_TO_ENTER, APP_VERSION_ID_TO_ENTER } from 'consts/messages';
 import { DynamicChoicesService } from 'services/dynamic-choices-service';
 import { getCurrentWorkingDirectory } from 'services/env-service';
 import { validateIfCanBuild } from 'services/files-service';
-import { getTasksForServerSide } from 'services/share/deploy';
+import { getTasksForClientSide, getTasksForServerSide } from 'services/share/deploy';
 import logger from 'utils/logger';
 import { addRegionToFlags, chooseRegionIfNeeded, getRegionFromString } from 'utils/region';
 
@@ -14,6 +14,7 @@ const MESSAGES = {
   appVersionId: APP_VERSION_ID_TO_ENTER,
   appId: APP_ID_TO_ENTER,
   force: 'Force push to live version',
+  cdn: 'Push files to CDN',
 };
 
 export default class Push extends AuthenticatedCommand {
@@ -43,17 +44,35 @@ export default class Push extends AuthenticatedCommand {
         char: 'f',
         description: MESSAGES.force,
       }),
+      cdn: Flags.boolean({
+        char: 'c',
+        description: MESSAGES.cdn,
+      }),
     }),
   );
 
   static args = {};
   DEBUG_TAG = 'code_push';
 
+  public async handleCdnUpload(directoryPath?: string): Promise<void> {
+    const { appVersionId } = await DynamicChoicesService.chooseAppAndAppVersion(false, false, {
+      autoSelectVersion: true,
+    });
+    logger.info('Deploying build to CDN...');
+    await getTasksForClientSide(Number(appVersionId), directoryPath || getCurrentWorkingDirectory()).run();
+  }
+
   public async run(): Promise<void> {
     const { flags } = await this.parse(Push);
-    const { directoryPath, region: strRegion } = flags;
+    const { directoryPath, region: strRegion, cdn } = flags;
     const region = getRegionFromString(strRegion);
     let appVersionId = flags.appVersionId;
+
+    if (cdn) {
+      await this.handleCdnUpload(directoryPath);
+      process.exit(0);
+    }
+
     validateIfCanBuild(directoryPath || getCurrentWorkingDirectory());
 
     try {
