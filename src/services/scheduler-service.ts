3c05b1e7a68762f86448aea05e690ba2c717d540
Maor Barazani <maorba@monday.com>
2025-04-20 17:37:18 +0300

    refactor scheduler service

--- src/services/scheduler-service.ts
+++ src/services/scheduler-service.ts
@@ -1,43 +1,17 @@
-import { StatusCodes } from 'http-status-codes';
-
 import { appSchedulerUrl } from 'src/consts/urls';
 
 import { execute } from './api-service';
+import {
+  CreateJobRequest,
+  CreateJobResponse,
+  ListJobsResponse,
+  SchedulerJob,
+  handleHttpErrors,
+} from './scheduler-service.utils';
 import { HttpError } from '../types/errors';
 import { AppId } from '../types/general';
-import { BaseResponseHttpMetaData, HttpMethodTypes } from '../types/services/api-service';
+import { HttpMethodTypes } from '../types/services/api-service';
 import { appsUrlBuilder } from '../utils/urls-builder';
-
-const handleHttpErrors = (error: HttpError) => {
-  switch (error.code) {
-    case StatusCodes.NOT_FOUND: {
-      throw new Error('monday-code deployment not found for the requested app');
-    }
-
-    case StatusCodes.FORBIDDEN: {
-      throw new Error('You are not authorized to access the requested app');
-    }
-
-    default: {
-      throw new Error('UNIMPLEMENTED ERROR CASE');
-    }
-  }
-};
-
-export type SchedulerJob = {
-  name: string;
-  schedule: string;
-  targetUrl: string;
-  retryConfig?: {
-    maxRetries: number;
-    minBackoffDuration: number;
-  };
-  timeout?: number;
-};
-
-export type ListJobsResponse = {
-  jobs: SchedulerJob[];
-} & BaseResponseHttpMetaData;
 
 export const listJobs = async (appId: AppId): Promise<SchedulerJob[]> => {
   try {
@@ -60,25 +34,13 @@ export const listJobs = async (appId: AppId): Promise<SchedulerJob[]> => {
   }
 };
 
-export type CreateJobRequest = {
-  schedule: string;
-  targetUrl: string;
-  retryConfig?: {
-    maxRetries?: number;
-    minBackoffDuration?: number;
-  };
-  timeout?: number;
-};
-
-export type CreateJobResponse = {
-  job: SchedulerJob;
-} & BaseResponseHttpMetaData;
-
-export const createJob = async (appId: AppId, job: CreateJobRequest): Promise<SchedulerJob> => {
+const createJob = async (appId: AppId, job: CreateJobRequest): Promise<SchedulerJob> => {
   try {
     const path = appSchedulerUrl(appId);
     const url = appsUrlBuilder(path);
 
+    console.log('&&&&&');
+    console.log(job);
     const response = await execute<CreateJobResponse>({
       url,
       headers: { Accept: 'application/json' },
@@ -96,30 +58,7 @@ export const createJob = async (appId: AppId, job: CreateJobRequest): Promise<Sc
   }
 };
 
-export const printJobs = (jobs: SchedulerJob[], logger = console.log): void => {
-  if (jobs.length === 0) {
-    logger('No scheduler jobs found.');
-    return;
-  }
-
-  logger('\nScheduler Jobs:');
-  for (const job of jobs) {
-    logger(`\n-----------------`);
-    logger(`Name: ${job.name}`);
-    logger(`Schedule: ${job.schedule}`);
-    logger(`Target URL: ${job.targetUrl}`);
-    if (job.retryConfig) {
-      logger(`Retry Config: ${job.retryConfig.maxRetries} retries, ${job.retryConfig.minBackoffDuration}s backoff`);
-    }
-
-    if (job.timeout) {
-      logger(`Timeout: ${job.timeout}s`);
-    }
-  }
-};
-
 export const SchedulerService = {
   listJobs,
   createJob,
-  printJobs,
 };
---
+++ src/services/scheduler-service.utils.ts
@@ -0,0 +1,83 @@
+import { StatusCodes } from 'http-status-codes';
+
+import logger from 'utils/logger';
+
+import { HttpError } from '../types/errors';
+import { BaseResponseHttpMetaData } from '../types/services/api-service';
+
+export type SchedulerJob = {
+  name: string;
+  schedule: string;
+  targetUrl: string;
+  retryConfig?: {
+    maxRetries: number;
+    minBackoffDuration: number;
+  };
+  timeout?: number;
+};
+
+export type ListJobsResponse = {
+  jobs: SchedulerJob[];
+} & BaseResponseHttpMetaData;
+
+export type CreateJobRequest = {
+  name: string;
+  description?: string;
+  schedule: string;
+  targetUrl: string;
+  retryConfig?: {
+    maxRetries?: number;
+    minBackoffDuration?: number;
+  };
+  timeout?: number;
+};
+
+export type CreateJobResponse = {
+  job: SchedulerJob;
+} & BaseResponseHttpMetaData;
+
+export const printJobs = (jobs: SchedulerJob[]): void => {
+  if (jobs.length === 0) {
+    logger.log('No scheduler jobs found.');
+    return;
+  }
+
+  logger.table(jobs);
+};
+
+export const validateCronExpression = (schedule: string): void => {
+  const cronRegex =
+    // eslint-disable-next-line unicorn/better-regex -- matching the backend regex which is correct and more strict in its format
+    /^(\*|([0-9]|1[0-9]|2[0-9]|3[0-1])|(\*\/([0-9]|1[0-9]|2[0-9]|3[0-1]))) (\*|([0-9]|1[0-9]|2[0-3])|(\*\/([0-9]|1[0-9]|2[0-3]))) (\*|([1-9]|1[0-9]|2[0-9]|3[0-1])|(\*\/([1-9]|1[0-9]|2[0-9]|3[0-1]))) (\*|([1-9]|1[0-2])|(\*\/([1-9]|1[0-2]))) (\*|([0-6])|(\*\/([0-6])))$/;
+  if (!cronRegex.test(schedule)) {
+    throw new Error('Invalid cronjob schedule format');
+  }
+};
+
+export const validateTargetUrl = (targetUrl: string): void => {
+  if (!targetUrl.startsWith('/')) {
+    throw new Error('Target URL must start with a slash');
+  }
+
+  // validate that the value after / is not an empty string
+  if (targetUrl.split('/')[1] === '') {
+    throw new Error('Target URL must not be empty');
+  }
+};
+
+export const handleHttpErrors = (error: HttpError) => {
+  switch (error.code) {
+    case StatusCodes.NOT_FOUND: {
+      throw new Error('monday-code deployment not found for the requested app');
+    }
+
+    case StatusCodes.FORBIDDEN: {
+      throw new Error('You are not authorized to access the requested app');
+    }
+
+    default: {
+      console.log(error); // FIXME: Maor: remove
+      throw new Error('UNIMPLEMENTED ERROR CASE');
+    }
+  }
+};
