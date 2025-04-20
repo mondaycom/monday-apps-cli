eff15f9a5dc4bdfb1918612661ed3ddb8424f5e2
Maor Barazani <maorba@monday.com>
2025-04-20 17:52:57 +0300

    remove duplicate code

--- test/cli-test-utils.ts
+++ test/cli-test-utils.ts
@@ -2,7 +2,7 @@ import { Command } from '@oclif/core';
 import axios from 'axios';
 import { ConfigService } from 'services/config-service';
 import { PromptService } from 'services/prompt-service';
-import { isDefined } from 'utils/guards';
+import { isDefined } from './utils/validations';
 
 const ANSI_REGEX = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
 
