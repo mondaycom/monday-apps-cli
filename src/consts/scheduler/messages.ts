32b9608afe5dd977b6813522a6c08710b51f6898
Maor Barazani <maorba@monday.com>
2025-06-08 23:06:56 +0300

    enhance name hint and patch version

--- src/consts/scheduler/messages.ts
+++ src/consts/scheduler/messages.ts
@@ -2,7 +2,7 @@ import { APP_ID_TO_ENTER } from 'src/consts/messages';
 
 export const SchedulerMessages = {
   appId: APP_ID_TO_ENTER,
-  name: 'Scheduled job name',
+  name: 'Scheduled job name (no whitespace)',
   description: 'Scheduled job description (optional)',
   schedule: 'Cron expression for the job schedule (relative to UTC)',
   targetUrl: 'Target URL path for the job (must start with /, will be relative to /mndy-cronjob)',
