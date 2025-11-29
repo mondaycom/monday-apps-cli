9638a0e830b7e63a7ea19698c2840a9ef1df0f02
Maor Barazani <maorba@monday.com>
2025-11-30 00:23:18 +0200

    add clientIp to db connection string request bump pkg version

--- src/services/database-service.ts
+++ src/services/database-service.ts
@@ -8,16 +8,28 @@ import { DatabaseConnectionStringResponseSchema } from 'types/services/database-
 import logger from 'utils/logger';
 import { appsUrlBuilder } from 'utils/urls-builder';
 
+const getPublicIp = async (): Promise<string> => {
+  const response = await fetch('https://checkip.amazonaws.com/');
+  if (!response.ok) {
+    throw new Error('Could not fetch public IP address.');
+  }
+
+  const ip = await response.text();
+  return ip.trim();
+};
+
 export const getDatabaseConnectionString = async (appId: AppId): Promise<DatabaseConnectionStringResponseSchema> => {
   const DEBUG_TAG = 'get_database_connection_string';
   try {
     const baseUrl = appStorageConnectionStringUrl(appId);
     const url = appsUrlBuilder(baseUrl);
+    const publicIp = await getPublicIp();
     const response = await execute<DatabaseConnectionStringResponseSchema>(
       {
         url,
         headers: { Accept: 'application/json' },
         method: HttpMethodTypes.GET,
+        query: { clientIp: publicIp },
       },
       databaseConnectionStringResponseSchema,
     );
